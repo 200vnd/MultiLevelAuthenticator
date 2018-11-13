d8436e77790faf2e4982d9de9d8b8c7229fb67a8
200vnd <nguyen00095@gmail.com>
2018-11-13 16:06:01 +0700

    Complete Text Password

--- app/src/main/java/com/anhnguyen/multilevelauthenticator/activity/MainActivity.java
+++ app/src/main/java/com/anhnguyen/multilevelauthenticator/activity/MainActivity.java
@@ -7,21 +7,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anhnguyen.multilevelauthenticator.R;
 import com.anhnguyen.multilevelauthenticator.model.Account;
 import com.anhnguyen.multilevelauthenticator.utils.HashMethods;
 import com.anhnguyen.multilevelauthenticator.utils.MyDatabaseHelper;
+import com.anhnguyen.multilevelauthenticator.utils.MyUtils;
 import com.facebook.stetho.Stetho;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btnChangeBehavior)
     Button btnChangeBehavior;
 
-    MyDatabaseHelper db = null;
+    private MyDatabaseHelper db = null;
+    private int flagSuccess = 0;
 
     private static final int TYPE_TEXT = 100;
     private static final int TYPE_PATTERN = 200;
@@ -51,12 +54,15 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         db = new MyDatabaseHelper(getApplicationContext());
-
+//        Account tester1 = new Account();
+//        tester1.setId("tester1");
+//        tester1.setPattern("5649646946563");
+//        db.addNewUser(tester1, TYPE_PATTERN);
     }
 
-
-    // check password
-    public void checkPasswordInDB(final String id, final int type) {
+    //---------------------------- start doing Text Password ---------------------------------
+    // check text password
+    private void checkTextPassword(final String id, String password, final int type) {
         // user not exists
         if (!db.checkID(id)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -66,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    registerNewUser(type);
+                    registerNewUser(type, false);
                 }
             });
             builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -78,22 +84,42 @@ public class MainActivity extends AppCompatActivity {
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
         }
+        // user exists password exists
+        else if (db.checkPasswordExistUser(id, type)) {
+            if (password.equals(db.getPassword(id, type))) {
+//                Toast.makeText(getApplicationContext(), "Authenticate successfully!", Toast.LENGTH_LONG).show();
+                MyUtils.ToastCustomSuccess(MainActivity.this);
+                flagSuccess = 1;
+            } else {
+//                Toast.makeText(getApplicationContext(), "Authentication failed! Wrong Password!", Toast.LENGTH_LONG).show();
+                Toasty.error(getApplicationContext(), "Authentication failed! Wrong password!", Toast.LENGTH_LONG, true).show();
+                flagSuccess = 0;
+            }
+        }
         // user exists no password
-    }
-
-    private void registerNewUser(int type) {
-
-        switch (type) {
-            case 100:
-                processNewTextPass(type);
-                break;
-            case 200:
-
-                break;
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This type of password is not set for this user!");
+            builder.setMessage("Do you want to register this password?");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    registerNewUser(type, true);
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         }
     }
 
-    private void processNewTextPass(final int type) {
+    private void processNewTextPass(final int type, final boolean userExists) {
         final Account user = new Account();
 
         final Dialog dialogNew = new Dialog(MainActivity.this);
@@ -135,14 +161,22 @@ public class MainActivity extends AppCompatActivity {
                 String passReTextPass = txtRePassNewTextPass.getText().toString();
                 String passReTextPassMD5 = HashMethods.md5(passReTextPass);
 
+
                 if (compareTextPass(passTextPassMD5, passReTextPassMD5)) {
                     user.setId(idTextPass);
                     user.setTextPass(passTextPassMD5);
-                    db.addNewUser(user, type);
-                    Toast.makeText(getApplicationContext(), "Added new user with text password", Toast.LENGTH_LONG).show();
-                    dialogNew.dismiss();
+                    if (userExists) {
+                        db.updatePassword(user.getId(), user.getTextPass(), type);
+                        Toasty.success(getApplicationContext(), "Added new password", Toast.LENGTH_LONG, true).show();
+                        dialogNew.dismiss();
+                    } else {
+                        db.addNewUser(user, type);
+                        Toasty.success(getApplicationContext(), "Added new user with text password", Toast.LENGTH_LONG, true).show();
+                        dialogNew.dismiss();
+                    }
+
                 } else {
-                    Toast.makeText(getApplicationContext(), "Password not matches. Try again!", Toast.LENGTH_LONG).show();
+                    Toasty.error(getApplicationContext(), "Password not matches. Try again!", Toast.LENGTH_LONG, true).show();
                 }
             }
         });
@@ -178,8 +212,10 @@ public class MainActivity extends AppCompatActivity {
                 String passTextPass = txtPassTestTextPass.getText().toString();
                 String passTextPassMD5 = HashMethods.md5(passTextPass);
 
-                Toast.makeText(getApplicationContext(), idTextPass, Toast.LENGTH_LONG).show();
-                checkPasswordInDB(idTextPass, TYPE_TEXT);
+                checkTextPassword(idTextPass, passTextPassMD5, TYPE_TEXT);
+                if (flagSuccess == 1) {
+                    dialogTest.dismiss();
+                }
             }
         });
 
@@ -193,6 +229,115 @@ public class MainActivity extends AppCompatActivity {
         dialogTest.show();
     }
 
+    private void showDialogChangeTextPass() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Change password for existing user or add new user?");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeTextPassMethod();
+            }
+        });
+        builder.setNegativeButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+                registerNewUser(TYPE_TEXT, false);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void changeTextPassMethod() {
+        final Dialog dialogChange = new Dialog(MainActivity.this);
+        dialogChange.setTitle("Change Text Password");
+        dialogChange.setContentView(R.layout.dialog_change_text_password);
+
+        Button btnConfirmChangeTextPass = dialogChange.findViewById(R.id.btnConfirmChangeTextPass);
+        Button btnCancelChangeTextPass = dialogChange.findViewById(R.id.btnCancelChangeTextPass);
+        final EditText txtIDChangeTextPass = dialogChange.findViewById(R.id.txtIDChangeTextPass);
+        final EditText txtPassChangeTextPass = dialogChange.findViewById(R.id.txtPassChangeTextPass);
+        final EditText txtRePassChangeTextPass = dialogChange.findViewById(R.id.txtRePassChangeTextPass);
+        final EditText txtOldPassChangeTextPass = dialogChange.findViewById(R.id.txtOldPassChangeTextPass);
+
+        txtPassChangeTextPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (txtPassChangeTextPass.length() >= 8) {
+                    dialogChange.findViewById(R.id.txtWarning8CharGood).setVisibility(View.VISIBLE);
+                    dialogChange.findViewById(R.id.txtWarning8CharBad).setVisibility(View.GONE);
+                } else {
+                    dialogChange.findViewById(R.id.txtWarning8CharGood).setVisibility(View.GONE);
+                    dialogChange.findViewById(R.id.txtWarning8CharBad).setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btnConfirmChangeTextPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idTextPass = txtIDChangeTextPass.getText().toString();
+                String oldTextPass = txtOldPassChangeTextPass.getText().toString();
+                final String oldTextPassMD5 = HashMethods.md5(oldTextPass);
+                String newTextPass = txtPassChangeTextPass.getText().toString();
+                String newTextPassMD5 = HashMethods.md5(newTextPass);
+                String reNewTextPass = txtRePassChangeTextPass.getText().toString();
+                String reNewTextPassMD5 = HashMethods.md5(reNewTextPass);
+
+                Log.d("changeTextPass", "id:" + idTextPass);
+                Log.d("changeTextPass", "id in db:" + db.checkID(idTextPass));
+                Log.d("changeTextPass", "old pass:" + oldTextPassMD5);
+                Log.d("changeTextPass", "old pass in db: "+ db.getPassword(idTextPass, TYPE_TEXT));
+                Log.d("changeTextPass", "new pass: "+ newTextPassMD5);
+                Log.d("changeTextPass", "new pass re: "+ reNewTextPassMD5);
+
+                if ((oldTextPassMD5.equals(db.getPassword(idTextPass, TYPE_TEXT))) && newTextPassMD5.equals(reNewTextPassMD5)) {
+                    db.updatePassword(idTextPass,newTextPassMD5,TYPE_TEXT);
+                    Toasty.success(getApplicationContext(), "Change password successfully!", Toast.LENGTH_LONG, true).show();
+                    dialogChange.dismiss();
+                } else if (!(oldTextPassMD5.equals(db.getPassword(idTextPass, TYPE_TEXT)))) {
+                    Toasty.error(getApplicationContext(), "Old password is different", Toast.LENGTH_LONG, true).show();
+                } else {
+                    Toasty.error(getApplicationContext(), "Password not matches. Try again!", Toast.LENGTH_LONG, true).show();
+                }
+            }
+        });
+
+        btnCancelChangeTextPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogChange.dismiss();
+            }
+        });
+
+        dialogChange.show();
+    }
+    //---------------------------- end doing Text Password ---------------------------------
+
+    private void registerNewUser(int type, boolean userExists) {
+
+        switch (type) {
+            case 100:
+                processNewTextPass(type, userExists);
+                break;
+            case 200:
+
+                break;
+        }
+    }
+
 
     @OnClick(R.id.btnTestTextPass)
     public void onBtnTestTextPassClicked() {
@@ -201,7 +346,9 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnChangeTextPass)
     public void onBtnChangeTextPassClicked() {
+        showDialogChangeTextPass();
     }
+
 
     @OnClick(R.id.btnTestPattern)
     public void onBtnTestPatternClicked() {
--- app/src/main/java/com/anhnguyen/multilevelauthenticator/utils/MyUtils.java
+++ app/src/main/java/com/anhnguyen/multilevelauthenticator/utils/MyUtils.java
@@ -1,4 +1,38 @@
 package com.anhnguyen.multilevelauthenticator.utils;
 
-public class MyUtils {
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.anhnguyen.multilevelauthenticator.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.target.DrawableImageViewTarget;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+
+public class MyUtils  {
+
+    public static void ToastCustomSuccess(Activity context) {
+        View toastView = context.getLayoutInflater().inflate(R.layout.toast_custom,
+                (ViewGroup) context.findViewById(R.id.toast_layout_root));
+
+        final ImageView imageView = toastView.findViewById(R.id.imgToastGif);
+        Glide.with(toastView).load(R.drawable.clap).into(new DrawableImageViewTarget(imageView));
+
+
+        Toast toast = new Toast(context.getApplicationContext());
+        // Set custom view in toast.
+        toast.setView(toastView);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0,0);
+        toast.show();
+    }
 }
